f77efc50674c5f71ad466c6f2f60c84e5ca3d9ee
mltrev23 <trevor.dev23@gmail.com>
2024-12-06 09:06:58 -0600

    feat: random tasks

--- template/validator/get_synapse.py
+++ template/validator/get_synapse.py
@@ -1,11 +1,10 @@
 import random
 import datasets
 from template.protocol import Challenge
-from template.utils.tasks import TASKS
+from template.validator.tasks import TASKS
 
 def get_synapse():
     task_name = random.choice(TASKS)
-    # task_name = 'abercrombie'
     
     dataset = datasets.load_dataset("nguha/legalbench", task_name)
     test_df = dataset["test"].to_pandas()
@@ -14,7 +13,6 @@ def get_synapse():
     rand_entry = test_df.sample(n=1)
     output = rand_entry['answer'].values[0]
     input = rand_entry.drop(columns=['answer']).to_dict(orient='records')[0]
-    # print(input)
     
     return Challenge(task_type=task_name, problem=input), output
 
